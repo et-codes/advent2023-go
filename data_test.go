94f51bffaa79e54e024cdb5affb3f9b752f23135
et-codes <ericthornton43@gmail.com>
2023-11-30 23:30:04 +0000

    Fix error message

--- data_test.go
+++ data_test.go
@@ -10,6 +10,6 @@ func TestGetDataFromFile(t *testing.T) {
 	got := a.ReadLines("README.md")
 	want := "# Advent of Code 2023"
 	if len(got) == 0 || got[0] != want {
-		t.Errorf("wanted %v, got %v", want, got[2])
+		t.Errorf("wanted %v, got %v", want, got[0])
 	}
 }
