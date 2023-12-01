08547da0f9bd47623aade4568bf79b89e056dfe7
et-codes <ericthornton43@gmail.com>
2023-12-01 05:33:00 +0000

    Part 1

--- day_01/day_1.go
+++ day_01/day_1.go
@@ -1,11 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+
+	a "github.com/et-codes/advent2023-go"
+)
 
 func main() {
 	fmt.Println(day_1())
 }
 
-func day_1() string {
-	return "Welcome to Advent of Code 2023!"
+func day_1() int {
+	input := a.ReadLines("day_1_data.txt")
+
+	sum := 0
+
+	for _, line := range input {
+		var first, last int
+
+		r := regexp.MustCompile(`\d`)
+		matches := r.FindAllString(line, -1)
+
+		first, _ = strconv.Atoi(matches[0])
+		if len(matches) > 1 {
+			last, _ = strconv.Atoi(matches[len(matches) - 1])
+		} else {
+			last = first
+		}
+		sum += first * 10 + last
+	}
+
+	return sum
 }
