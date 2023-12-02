29fbf30512fd340d0b78d0350aeea7f70c906d4b
et-codes <ericthornton43@gmail.com>
2023-12-02 21:25:10 +0000

    Update project setup

---
+++ day_03/day_03.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const (
+	test   = "day_2_test_data.txt"
+	puzzle = "day_2_data.txt"
+)
+
+func main() {
+	fmt.Println(day03(puzzle))
+}
+
+func day03(path string) []int {
+	return []int{0, 0}
+}
