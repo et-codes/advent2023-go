d2b416a1c0c4c1a1e503a756df9f3532f2215534
et-codes <ericthornton43@gmail.com>
2023-12-02 18:40:36 +0000

    Part 2

--- day_02/day_2.go
+++ day_02/day_2.go
@@ -9,19 +9,29 @@ import (
 	a "github.com/et-codes/advent2023-go"
 )
 
-var bag = map[string]int{
-	"red":   12,
-	"green": 13,
-	"blue":  14,
-}
+type bag map[string]int
+
+const (
+	test   = "day_2_test_data.txt"
+	puzzle = "day_2_data.txt"
+)
+
+var (
+	bag1 = bag{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+)
 
 func main() {
-	fmt.Println(day_2("day_2_data.txt"))
+	fmt.Println(day_2(puzzle))
 }
 
-func day_2(path string) int {
+func day_2(path string) []int {
 	games := a.ReadLines(path)
-	result := 0
+
+	var result1, result2 int
 
 	for _, game := range games {
 		re := regexp.MustCompile(`\d+`)
@@ -30,25 +40,35 @@ func day_2(path string) int {
 		rounds := strings.Split(game, ": ")
 		rounds = strings.Split(rounds[1], "; ")
 
+		bag2 := bag{}
 		possible := true
 		for _, round := range rounds {
 			cubes := strings.Split(round, ", ")
-
 			for _, cube := range cubes {
 				re = regexp.MustCompile(`(\d+|red|green|blue)`)
 				matches := re.FindAllString(cube, -1)
 				qty, _ := strconv.Atoi(matches[0])
 				color := matches[1]
-				if qty > bag[color] {
+
+				if qty > bag2[color] {
+					bag2[color] = qty
+				}
+
+				if qty > bag1[color] {
 					possible = false
-					break
 				}
 			}
 		}
 
 		if possible {
-			result += gameNumber
+			result1 += gameNumber
 		}
+
+		result2 += power(bag2)
 	}
-	return result
+	return []int{result1, result2}
+}
+
+func power(bag bag) int {
+	return bag["red"] * bag["blue"] * bag["green"]
 }
--- day_02/day_2_test.go
+++ day_02/day_2_test.go
@@ -1,20 +1,23 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestDay2(t *testing.T) {
 	tests := map[string]struct {
 		source string
-		want   int
+		want   []int
 	}{
-		"with test data":   {"day_2_test_data.txt", 8},
-		"with puzzle data": {"day_2_data.txt", 2776},
+		"with test data":   {test, []int{8, 2286}},
+		"with puzzle data": {puzzle, []int{2776, 68638}},
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			got := day_2(test.source)
-			if got != test.want {
+			if !reflect.DeepEqual(got, test.want) {
 				t.Errorf("got %d, want %d", got, test.want)
 			}
 		})
